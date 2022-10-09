2448a682abf12eaa5b8bccae403cea7425f2adea
Brendan Duke <brendanw.duke@gmail.com>
2022-10-09 13:58:21 -0400

    Add Shortest Path initial skeleton

---
+++ src/codeforces/shortestpath.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int num_cities;
+  std::cin >> num_cities;
+  int num_roads;
+  std::cin >> num_roads;
+  int num_forbidden_triplets;
+  std::cin >> num_forbidden_triplets;
+}
